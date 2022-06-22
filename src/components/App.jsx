7df25df2e5cde2b060420d4a55839293d3d3b02e
rohitrj29 <f20200906@pilani.bits-pilani.ac.in>
2022-06-22 22:54:20 +0530

    modify

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,12 +1,50 @@
-import React from "react";
-import Form from "./Form";
-
-var userIsRegistered = true;
+import React, { useState } from "react";
 
 function App() {
+  const [contact, setContact] = useState({
+    fName: "",
+    lName: "",
+    email: ""
+  });
+ function handleChange(){
+   const{value,name}= event.target;
+  setContact(prevValue =>{
+    if(name==='fName'){
+      return{
+        fName:value,
+        lName:prevValue.lName,
+        email:prevValue.email
+      }
+    }
+    else if(name==='lName'){
+      return{
+        fName:prevValue.fName,
+        lName:value,
+        email:prevValue.email
+      }
+    }
+    if(name==='email'){
+      return{
+        fName:prevValue.fName,
+        lName:prevValue.lName,
+        email:value
+      }
+    }
+  });
+ }
+
   return (
     <div className="container">
-      <Form isRegistered={userIsRegistered} />
+      <h1>
+        Hello {contact.fName} {contact.lName}
+      </h1>
+      <p>{contact.email}</p>
+      <form>
+        <input name="fName" onChange={handleChange} placeholder="First Name" value={contact.fName} />
+        <input name="lName" onChange={handleChange} placeholder="Last Name" value={contact.lName} />
+        <input name="email" onChange={handleChange} placeholder="Email" value={contact.email} />
+        <button>Submit</button>
+      </form>
     </div>
   );
 }
